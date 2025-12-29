cb2ee4a1b962808c4bbf0db673fa3f651c59f3d4
Anerti <e.andrianombana@protonmail.com>
2025-12-29 13:50:48 +0300

    feat: adding getProductsByCriteria method with tests

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,16 +3,38 @@ package org.example;
 import org.example.db.DBConnection;
 import org.example.service.DataRetriever;
 
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Main {
+    public static Instant formater(String date){
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        LocalDate localDate = LocalDate.parse("2024-02-01", formatter);
+        ZonedDateTime output = localDate.atStartOfDay(ZoneOffset.UTC);
+        return output.toInstant();
+    }
+
     public static void main(String[] args) {
         DBConnection connection = new DBConnection();
         DataRetriever dataRetriever = new DataRetriever(connection);
 
-        //System.out.println(dataRetriever.getAllCategories());
+        System.out.println(dataRetriever.getAllCategories());
 
-        //System.out.println(dataRetriever.getProductList(1, 10));
-        //System.out.println(dataRetriever.getProductList(1, 5));
-        //System.out.println(dataRetriever.getProductList(1, 3));
+        System.out.println(dataRetriever.getProductList(1, 10));
+        System.out.println(dataRetriever.getProductList(1, 5));
+        System.out.println(dataRetriever.getProductList(1, 3));
         System.out.println(dataRetriever.getProductList(2, 2));
+
+        System.out.println(dataRetriever.getProductsByCriteria("Dell", null, null, null));
+        System.out.println(dataRetriever.getProductsByCriteria(null, "info", null, null));
+        System.out.println(dataRetriever.getProductsByCriteria("iPhone", "mobile", null, null));
+        System.out.println(dataRetriever.getProductsByCriteria(null, null, formater("2024-02-01"), formater("2024-03-01")));
+        System.out.println(dataRetriever.getProductsByCriteria("Samsung", "bureau", null, null));
+        System.out.println(dataRetriever.getProductsByCriteria("Sony", "informatique", null, null));
+        System.out.println(dataRetriever.getProductsByCriteria(null, "audio", formater("2024-02-01"), formater("2024-12-01")));
+        System.out.println(dataRetriever.getProductsByCriteria(null, null, null, null));
     }
 }
--- src/main/java/org/example/service/DataRetriever.java
+++ src/main/java/org/example/service/DataRetriever.java
@@ -4,10 +4,7 @@ import org.example.db.DBConnection;
 import org.example.model.Category;
 import org.example.model.Product;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
@@ -86,8 +83,65 @@ public class DataRetriever {
         }
     }
 
-    List<Product> getProductsByCriteria(String productName, String categoryName, Instant creationMin, Instant creationMax){
-        throw new UnsupportedOperationException("Not supported yet.");
+    public List<Product> getProductsByCriteria(String productName, String categoryName, Instant creationMin, Instant creationMax){
+        final StringBuilder query = new StringBuilder
+                ("""
+                    SELECT
+                        product.id AS product_id,
+                        product.name AS product_name,
+                        product_category.id AS category_id,
+                        product_category.name AS category,
+                        product.creation_datetime AS product_creation_date
+                    FROM
+                        product_management_app.product
+                    INNER JOIN
+                        product_management_app.product_category
+                    ON
+                        product.id = product_category.product_id
+                    WHERE 1 = 1
+                """);
+        List<Product> products = new ArrayList<>();
+
+        if (productName != null && !productName.isEmpty()) query.append(" AND product.name ILIKE ?");
+
+        if (categoryName != null && !categoryName.isEmpty()) query.append(" AND product_category.name ILIKE ?");
+
+        if (creationMin != null) query.append(" AND creation_datetime >= ?");
+
+        if (creationMax != null) query.append(" AND creation_datetime <= ?");
+
+        query.append(";");
+
+        try (Connection c = connection.getConnection()){
+            PreparedStatement ps = c.prepareStatement(query.toString());
+            int paramIndex = 1;
+            if (productName != null && !productName.isEmpty()) ps.setString(paramIndex++, String.format("%%%s%%", productName));
+
+            if (categoryName != null && !categoryName.isEmpty()) ps.setString(paramIndex++, String.format("%%%s%%", categoryName));
+
+            if (creationMin != null) ps.setTimestamp(paramIndex++, Timestamp.from(creationMin));
+
+            if (creationMax != null) ps.setTimestamp(paramIndex, Timestamp.from(creationMax));
+
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                products.add(
+                        new Product(
+                                rs.getInt("product_id"),
+                                rs.getString("product_name"),
+                                rs.getTimestamp("product_creation_date").toInstant(),
+                                new Category(
+                                        rs.getInt("category_id"),
+                                        rs.getString("category")
+                                )
+                        )
+                );
+            }
+            return products;
+        }
+        catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     List<Product> getProductsByCriteria(String productName, String categoryName, Instant creationMin, Instant creationMax, int page, int size){
